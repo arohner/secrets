3abb150367061fd7b1381e7582e4e4aa1538fb9d
Allen Rohner <arohner@gmail.com>
2014-07-02 14:50:14 -0500

    Namespace the name, to be friendlier

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject secrets "0.1.0"
+(defproject arohner/secrets "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[me.raynes/conch "0.8.0"]
